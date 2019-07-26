8215def9ebb4faa5362c3aecb4775c7deaab54d6
maddiestrange <maddie.strange@gmail.com>
2019-07-26 11:53:48 -0500

    MVP completed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 import CharacterCard from './CharacterCard'
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  
   const [characters, setCharacters] = useState([]);
   useEffect(() => {
-    // TODO: Add AJAX/API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
       axios
         .get('https://rickandmortyapi.com/api/character/')
         .then(response => {
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -5,8 +5,6 @@ import LocationCard from './LocationCard'
 export default function LocationsList() {
     const [locations, setLocations] = useState([]);
     useEffect(() => {
-      // TODO: Add AJAX/API Request here - must run in `useEffect`
-      //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
         axios
           .get('https://rickandmortyapi.com/api/location/')
           .then(response => {
